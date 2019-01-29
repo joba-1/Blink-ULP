e2715beb271698891f0e73037f7667d2342b8a33
Joba-1 <joban123.psn@gmail.com>
2019-01-29 16:54:59 +0100

    pin config inside ulp

--- main/main.c
+++ main/main.c
@@ -5,16 +5,13 @@
  */
 
 #include <stdio.h>         // printf(), fflush()
-#include "ulp-util.h"      // ulp_init(), ulp_start()
-#include "driver/rtc_io.h" // rtc_gpio_init()
 #include "esp_sleep.h"     // esp_sleep_enable_ulp_wakeup(), esp_deep_sleep_start()
+
+#include "ulp-util.h"      // my ulp_init(), ulp_start()
 
 void app_main()
 {
   printf("Hello Blink ULP!\n");
-
-  // Would like to do this in ULP but failed so far...
-  rtc_gpio_init(GPIO_NUM_2); 
 
   ulp_init();
   ulp_start();
